24f6ec38d4ec588e7ffc7898a11a38e57d13ce52
v-prusakov <v-prusakov@yandex-team.ru>
2018-01-11 15:12:09 +0300

    item deletion

--- src/components/crm/crm.vue
+++ src/components/crm/crm.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="CRM">
-		<h1>CRM</h1>
+		<h1 class="display-1">CRM</h1>
 		<v-text-field
 		prepend-icon="search"
 		label="Search"
@@ -38,6 +38,7 @@
 				<template slot="footer">
 					<td colspan="100%">
 						<v-btn color="primary">Add user</v-btn>
+						<v-btn color="error" v-if="hasSelectedItems" @click="deleteSelected()">Delete selected</v-btn>
 					</td>
 				</template>
 			</v-data-table>
@@ -47,87 +48,114 @@
 
 <script>
   export default {
-    data () {
-      return {
-      	selected: [],
-      	search: '',
-        headers: [
-          {
-            text: '#',
-            align: 'left',
-            value: 'order'
-          },
-          { text: 'Status', value: 'status' },
-          { text: 'Name', value: 'name' },
-          { text: 'Email', value: 'email' },
-          { text: 'Phone', value: 'phone' },
-          { text: 'Assigned to', value: 'assigned' },
-          { text: 'Salutation', value: 'salutation' },
-          { text: 'Suffix', value: 'suffix' }
-        ],
-        items: [
-          {
-            order: 1,
-            status: 'Offline',
-            name: 'Franz',
-            email: 'franz@gmail.com',
-            phone: 7777777,
-            assigned: 'writers',
-            salutation: 'Sartre',
-            suffix: 'Absurd'
-          },
-          {
-            order: 2,
-            status: 'Online',
-            name: 'asds',
-            email: 'fs@gmail.com',
-            phone: 3434,
-            assigned: 'sds',
-            salutation: 'gf',
-            suffix: 'fg'
-          },
-          {
-            order: 3,
-            status: 'Online',
-            name: '3asdfds',
-            email: 'fs@gmail.com',
-            phone: 3434,
-            assigned: 'sds',
-            salutation: 'gf',
-            suffix: 'fg'
-          },
-          {
-            order: 4,
-            status: 'Online',
-            name: '4afsds',
-            email: 'fs@gmail.com',
-            phone: 3434,
-            assigned: 'sds',
-            salutation: 'gf',
-            suffix: 'fg'
-          },
-          {
-            order: 5,
-            status: 'Online',
-            name: '5asds',
-            email: 'fs@gmail.com',
-            phone: 3434,
-            assigned: 'sds',
-            salutation: 'gf',
-            suffix: 'fg'
-          },
-          {
-            order: 6,
-            status: 'Online',
-            name: '6asds',
-            email: 'fs@gmail.com',
-            phone: 3434,
-            assigned: 'sds',
-            salutation: 'gf',
-            suffix: 'fg'
-          }
-        ]
-      }
-    }
+	data () {
+		return {
+			selected: [],
+			search: '',
+			headers: [
+				{
+				text: '#',
+				align: 'left',
+				value: 'order'
+				},
+				{ text: 'Status', value: 'status' },
+				{ text: 'Name', value: 'name' },
+				{ text: 'Email', value: 'email' },
+				{ text: 'Phone', value: 'phone' },
+				{ text: 'Assigned to', value: 'assigned' },
+				{ text: 'Salutation', value: 'salutation' },
+				{ text: 'Suffix', value: 'suffix' }
+			],
+			items: [
+				{
+				order: 1,
+				status: 'Offline',
+				name: 'Franz',
+				email: 'franz@gmail.com',
+				phone: 7777777,
+				assigned: 'writers',
+				salutation: 'Sartre',
+				suffix: 'Absurd'
+				},
+				{
+				order: 2,
+				status: 'Online',
+				name: 'asds',
+				email: 'fs@gmail.com',
+				phone: 3434,
+				assigned: 'sds',
+				salutation: 'gf',
+				suffix: 'fg'
+				},
+				{
+				order: 3,
+				status: 'Online',
+				name: '3asdfds',
+				email: 'fs@gmail.com',
+				phone: 3434,
+				assigned: 'sds',
+				salutation: 'gf',
+				suffix: 'fg'
+				},
+				{
+				order: 4,
+				status: 'Online',
+				name: '4afsds',
+				email: 'fs@gmail.com',
+				phone: 3434,
+				assigned: 'sds',
+				salutation: 'gf',
+				suffix: 'fg'
+				},
+				{
+				order: 5,
+				status: 'Online',
+				name: '5asds',
+				email: 'fs@gmail.com',
+				phone: 3434,
+				assigned: 'sds',
+				salutation: 'gf',
+				suffix: 'fg'
+				},
+				{
+				order: 6,
+				status: 'Online',
+				name: '6asds',
+				email: 'fs@gmail.com',
+				phone: 3434,
+				assigned: 'sds',
+				salutation: 'gf',
+				suffix: 'fg'
+				}
+			]
+		}
+	},
+	computed: {
+		hasSelectedItems() {
+			return this.selected.length > 0;
+		}
+	},
+	methods: {
+		showSelected() {
+			if (!this.hasSelectedItems) {
+				return;
+			}
+		},
+		deleteSelected() {
+			const indexesToRemove = this.selected.map(item => {
+				return this.items.findIndex(arrayItem => arrayItem.order === item.order);
+			}).sort((a, b) => a - b);
+
+			this.$store.commit('setAlert',
+				{type: 'success', message: 'Items successufly deleted'}
+			);
+
+			for (let i = indexesToRemove.length - 1; i >= 0; i--) {
+				this.items.splice(indexesToRemove[i], 1);
+			}
+
+			this.selected = [];
+		}
+	}
   }
 </script>
